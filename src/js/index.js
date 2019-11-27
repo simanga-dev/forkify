a6375eb30e923ba324eaf3e4e1d31e7c271c9612
Henkho Dryza <simangahk@gmail.com>
2019-11-26 23:48:39 -0800

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// Global app controller
+
+import number from './test'
+
+const x = 26
+
+console.log(`I imported this number okay okay ${number} from another module, our variables is ${x}`)
---
+++ src/js/test.js
@@ -0,0 +1,3 @@
+console.log('imprted module')
+
+export default 99
