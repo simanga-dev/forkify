dcfa801ed7269bac5b6b21b543567f4d2a2549a2
Henkho Dryza <simangahk@gmail.com>
2020-07-11 14:53:34 -0700

    project now use TS

--- src/js/index.js
+++
@@ -1,3 +0,0 @@
-// globak js style
-import x from "./test";
-console.log("I import num from another module", x);
---
+++ src/js/index.ts
@@ -0,0 +1,3 @@
+// globak js style
+// import x from "./test";
+console.log("welcome to typescript");
